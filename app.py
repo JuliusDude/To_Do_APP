5e3f264485c3910e8161493df38e656427a16621
Julius <juliusbthomas10@gmail.com>
2025-08-28 02:06:03 +0530

    Used Gitignore

---
+++ app.py
@@ -0,0 +1,68 @@
+# Imports 
+from flask import Flask, render_template, redirect,request
+from flask_scss import Scss
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+# My app
+app = Flask(__name__)
+
+# Enable SCSS (compiles SCSS in /static/scss → CSS in /static/css)
+Scss(app, static_dir='static', asset_dir='static/scss')
+
+# Setup database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+
+class MyTask(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    content = db.Column(db.String(100), nullable = False)
+    complete = db.Column(db.Integer,default = 0)
+    created = db.Column(db.DateTime, default = datetime.utcnow )
+    
+    def __repr__(self):
+        return f"Task {self.id}"
+    
+
+
+
+
+@app.route("/",methods=["POST","GET"])
+def index():
+    if request.method == "POST":
+        current_task = request.form['content']
+        new_task = MyTask(content=current_task)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except Exception as e:
+            print(f"Error :{e}")
+            return f"Error {e}"
+        
+    else:
+        tasks = MyTask.query.order_by(MyTask.created).all()
+        return render_template("index.html",tasks=tasks)
+
+
+
+
+
+    return render_template("index.html")
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
